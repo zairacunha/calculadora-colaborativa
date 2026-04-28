45e71b57db2224b19032eb2d67fa5082fce6be9b
MarcoPazMartinez <marcomartinez11155@gmail.com>
2026-04-27 21:42:57 -0300

    Agregar función de resta con opción en menú

--- calculadora.py
+++ calculadora.py
@@ -3,10 +3,15 @@ def sumar(a, b):
     """Suma dos números."""
     return a + b
 
+def restar(a, b):
+    """Resta dos números."""
+    return a - b
+
 def mostrar_menu():
     """Muestra el menú de opciones."""
     print("\n=== Calculadora Colaborativa ===")
     print("1. Sumar")
+    print("2. restar")
     print("0. Salir")
 
 def main():
@@ -21,7 +26,15 @@ def main():
             a = float(input("Primer número: "))
             b = float(input("Segundo número: "))
             print(f"Resultado: {a} + {b} = {sumar(a, b)}")
+
+        elif opcion == '2':
+            a = float(input("Primer número: "))
+            b = float(input("Segundo número: "))
+            print(f"Resultado: {a} - {b} = {restar(a, b)}")
+            
         else:
             print("⚠️ Opción no válida.")
 if __name__ == "__main__":
     main()
+    
+ 
