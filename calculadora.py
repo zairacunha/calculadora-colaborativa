f9f3a13552e4c0107fdfa47d92ad9d5bd8bedb21
Zaira Cunha <zairacunhadasilva@gmail.com>
2026-05-18 21:30:21 -0300

    Archivo calculadora.py actualizada con nueva información

--- calculadora.py
+++ calculadora.py
@@ -7,11 +7,16 @@ def restar(a, b):
     """Resta dos números."""
     return a - b
 
+def multiplicar(a, b):
+    """Multiplica dos números."""
+    return a * b
+
 def mostrar_menu():
     """Muestra el menú de opciones."""
     print("\n=== Calculadora Colaborativa ===")
     print("1. Sumar")
-    print("2. restar")
+    print("2. Restar")
+    print("3. Multiplicar")
     print("0. Salir")
 
 def main():
@@ -31,10 +36,14 @@ def main():
             a = float(input("Primer número: "))
             b = float(input("Segundo número: "))
             print(f"Resultado: {a} - {b} = {restar(a, b)}")
+        
+        elif opcion == '3':
+            a = float(input("Primer número: "))
+            b = float(input("Segundo número: "))
+            print(f"Resultado: {a} * {b} = {multiplicar(a, b)}")
             
         else:
             print("⚠️ Opción no válida.")
+
 if __name__ == "__main__":
     main()
-    
- 
